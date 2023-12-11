6adf097a4b4d24aacf80eec171bd16c7cdd79d51
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2023-12-12 03:38:31 +0600

    Create string_math_operator.php

---
+++ string_math_operator.php
@@ -0,0 +1,90 @@
+<?php
+// string length
+     $string = "I am full stack web developer";
+
+    //  echo strlen($string);
+ 
+    //  word count
+
+    $string = "I am full stack web developer";
+
+    // echo str_word_count($string);
+
+
+
+
+       // reverse text
+
+       $string = "I am full stack web developer";
+
+    //    echo strrev($string);
+
+
+      // searching string possion
+      $string = "I am full stack web developer";
+
+     
+       //    echo strrev($string);
+
+
+      //  string replece
+      $string = "I am full stack web developer";
+
+        //   echo str_replace('I','Rakib',$string);
+
+
+
+        // Math Part
+
+        // echo pi();
+
+    //    echo max(10,20,3,065,25,42,42) ;
+    // echo min(10,20,3,065,25,42,42) ;
+
+    // + positive number means absolute
+    //  echo abs(-20);
+
+// root
+    // echo sqrt(2);
+
+    // squre
+
+    // echo pow(3,2);or 3**3
+
+    // round , ceil , floor
+
+    $price = 50.80;
+    // echo round($price);  .49 ar nica 50 .5 uppor 51
+    // echo ceil($price);  51 hoba 
+    // echo floor($price); 50 value 
+
+
+    // otp full form one time password
+
+    // echo rand(100000,999999);
+
+
+
+      
+
+   /*  PHP Operators
+Operators are used to perform operations on variables and values.
+
+PHP divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Increment/Decrement operators
+Logical operators
+String operators
+Array operators
+Conditional assignment operators
+*/
+
+
+$number = 20;
+$num2 = 20;
+ 
+echo $number**$num2;
+?>
