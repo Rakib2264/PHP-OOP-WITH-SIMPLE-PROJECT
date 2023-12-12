0df597efaf99b03ed8be236c22e4c1ba33a5ca86
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2023-12-12 23:39:25 +0600

    switch

---
+++ condition.php
@@ -0,0 +1,51 @@
+ <?php     
+    //  $x = 10;  
+    // echo ++$x;
+    // echo '<br>';
+    // echo $x++;
+
+    // if elseif else
+
+
+//     function calculateGrade($marks) {
+//         if ($marks >= 90) {
+//             return 'A+';
+//         } elseif ($marks >= 80) {
+//             return 'A';
+//         } elseif ($marks >= 70) {
+//             return 'B';
+//         } elseif ($marks >= 60) {
+//             return 'C';
+//         } elseif ($marks >= 50) {
+//             return 'D';
+//         } else {
+//             return 'F';
+//         }
+//     }
+    
+//     // Example usage
+//     $studentMarks = 95;
+//    echo  calculateGrade($studentMarks);
+    
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
+?> 
+
+ 
+
+
+ 
--- operator.php
+++ operator.php
@@ -13,5 +13,4 @@
       }else{
          echo "false";
       }
-   
    ?>
