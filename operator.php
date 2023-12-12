f38a944419f466a5e0df4799ce9b945589147beb
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2023-12-12 20:24:52 +0600

    Create operator.php
    importent

---
+++ operator.php
@@ -0,0 +1,17 @@
+<?php
+// comparison ==, ===, !=, !==, >, <, >=, <=
+// == equal chcek
+// === equal and type chcek
+// != not equal chcek
+// !== not equal and type chcek
+// > grater then
+// < less then
+   $num1='10';
+   $num2=10;
+   if($num1!==$num2){
+      echo "true";
+   }else{
+      echo "false";
+   }
+
+?>
