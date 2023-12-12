f771cb5c817500a4b6344cca2d92a2430c8546a7
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2023-12-12 20:30:58 +0600

    Update operator.php

--- operator.php
+++ operator.php
@@ -1,9 +1,9 @@
    <?php
    // comparison ==, ===, !=, !==, >, <, >=, <=
    // == equal chcek
-   // === equal and type chcek
+   // === equal and data type chcek
    // != not equal chcek
-   // !== not equal and type chcek
+   // !== not equal and data type chcek
    // > grater then
    // < less then
       $num1='10';
