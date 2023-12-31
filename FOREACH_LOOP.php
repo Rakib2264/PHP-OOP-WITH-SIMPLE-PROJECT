1d5ad995602048fb7ec7aca9d243474d488a692c
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2023-12-31 22:18:56 +0600

    Create FOREACH_LOOP.php
    Array

---
+++ FOREACH_LOOP.php
@@ -0,0 +1,12 @@
+<?php
+//  Array one veriable many data and this backet[]
+$colors = array("red", "green", "blue", "yellow"); 
+$data = ['rakib','khan'];
+$dataa = ['rakib'=>'Bro'];
+print_r($dataa);
+foreach ($dataa as $x) {
+  print_r("$x <br>");
+}
+
+  
+?>
