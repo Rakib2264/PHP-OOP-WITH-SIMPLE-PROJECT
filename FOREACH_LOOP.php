597c8b9e284f0c2a0be17bb73e03acd6f40c589f
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2023-12-31 23:58:23 +0600

    Update FOREACH_LOOP.php

--- FOREACH_LOOP.php
+++ FOREACH_LOOP.php
@@ -3,8 +3,7 @@
 $colors = array("red", "green", "blue", "yellow"); 
 $data = ['rakib','khan'];
 $dataa = ['rakib'=>'Bro'];
-print_r($dataa);
-foreach ($dataa as $x) {
+ foreach ($dataa as $x) {
   print_r("$x <br>");
 }
 
