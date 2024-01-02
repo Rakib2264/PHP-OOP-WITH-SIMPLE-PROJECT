e595f15d614a4637e54eaf4ca573fd9b5d57a7d3
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2024-01-02 22:36:48 +0600

    Update FOREACH_LOOP.php

--- FOREACH_LOOP.php
+++ FOREACH_LOOP.php
@@ -1,11 +1,20 @@
 <?php
 //  Array one veriable many data and this backet[]
-$colors = array("red", "green", "blue", "yellow"); 
-$data = ['rakib','khan'];
-$dataa = ['rakib'=>'Bro'];
- foreach ($dataa as $x) {
-  print_r("$x <br>");
-}
+// $colors = array("red", "green", "blue", "yellow"); 
+// $data = ['rakib','khan'];
+// $dataa = ['rakib'=>'Bro'];
+//  foreach ($dataa as $x) {
+//   print_r("$x <br>");
+// }
 
-  
+$data = [
+  'class'=>'class one',
+  'section'=> [
+    'section A'=>[
+      'students'=>['rakib','robin','hadi'],
+    ],
+  ],
+ ];
+echo "<pre>";
+print_r($data);
 ?>
