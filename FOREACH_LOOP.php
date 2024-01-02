43ce750e663e87a023817c89c99283a97c754f1b
Rakib2264 <88357704+Rakib2264@users.noreply.github.com>
2024-01-02 23:16:28 +0600

    foreach
    $students = [
      [
        "brand"=>"Ford",
      "model"=>"Mustang",
      "year"=>1964
      ],
    
    [
      "brand"=>"Computer",
      "model"=>"la",
      "year"=>2058
    ],
    [
      "brand"=>"Laptop",
      "model"=>"la",
      "year"=>2089
    ]
    
    ];
    
    foreach ($students as $key => $student) {
      foreach ($student as $key => $student) {
        print_r($key.' = '.$student.'<br>');
      }
      echo '<hr>';
    }

--- FOREACH_LOOP.php
+++ FOREACH_LOOP.php
@@ -1,20 +1,57 @@
 <?php
 //  Array one veriable many data and this backet[]
-// $colors = array("red", "green", "blue", "yellow"); 
-// $data = ['rakib','khan'];
-// $dataa = ['rakib'=>'Bro'];
+$colors = array("red", "green", "blue", "yellow"); 
+$data = ['rakib','khan'];
+$dataa = ['rakib','robin','hadi','robin','hadi','robin','hadi','robin','hadi'];
 //  foreach ($dataa as $x) {
 //   print_r("$x <br>");
 // }
 
-$data = [
-  'class'=>'class one',
-  'section'=> [
-    'section A'=>[
-      'students'=>['rakib','robin','hadi'],
-    ],
+// for ($i=0; $i < count($dataa); $i++) { 
+//   print_r("$dataa[$i] <br>");
+// }
+
+// $data = [
+//   'class'=>'class one',
+//   'section'=> [
+//     'section A'=>[
+//       'students'=>['rakib','robin','hadi'],
+//     ],
+//   ],
+//  ];
+// echo "<pre>";
+// print_r($data);
+// 
+
+// Assositive array loop
+
+$students = [
+  [
+    "brand"=>"Ford", 
+  "model"=>"Mustang", 
+  "year"=>1964
   ],
- ];
-echo "<pre>";
-print_r($data);
+
+[
+  "brand"=>"Computer", 
+  "model"=>"la", 
+  "year"=>2058
+],
+[
+  "brand"=>"Laptop", 
+  "model"=>"la", 
+  "year"=>2089
+]
+
+];
+
+foreach ($students as $key => $student) {
+  foreach ($student as $key => $student) {
+    print_r($key.' = '.$student.'<br>');
+  }
+  echo '<hr>';
+}
+
+
+
 ?>
